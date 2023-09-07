c5ea316190e46444e8f3ac5f6cc6757b8714cf79
İsmail <idincer944@gmail.com>
2023-09-07 21:48:03 +0300

    Added hamburger manu functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello world!')
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamb');
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }    
+});

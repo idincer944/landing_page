4e4347ad204c1366463178c4db8eb1fc7f8bf363
İsmail <idincer944@gmail.com>
2023-09-07 22:26:34 +0300

    Added fade-in and fade-out animations for hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,17 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
-btnHamburger.addEventListener('click', function(){
+btnHamburger.addEventListener('click', function(){ // Close the hamburger menu
     console.log('open hamb');
 
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
-    } else {
-        btnHamburger.classList.add('open');
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else { // open the hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }    
 });
